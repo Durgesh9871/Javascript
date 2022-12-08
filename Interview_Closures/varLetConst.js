32ed385fbf5ca28491571e2e55342550089e7c30
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-08 13:25:21 +0530

    file

---
+++ Interview_Closures/varLetConst.js
@@ -0,0 +1,14 @@
+
+
+console.log(b)
+
+let a = 10  
+var b = 100 
+
+
+// if we do console.log(a) we cannot access it, write , we can only access it after initialization . 
+
+
+var c = 200 
+var c = 2000 
+console.log("c" , c)
