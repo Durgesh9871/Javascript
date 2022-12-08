1fad40052d5511e2d22daf483c26d457e85e86c9
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-08 17:51:56 +0530

    file

--- Interview_Closures/varLetConst.js
+++ Interview_Closures/varLetConst.js
@@ -12,3 +12,17 @@ var b = 100
 var c = 200 
 var c = 2000 
 console.log("c" , c)
+
+
+//  But in terms of let its saying no duplicates decleration .... 
+//  It gives syntax error  :----- 
+
+// for example  :- Syntax error  
+
+// let z = 1 ; 
+// let z = 2 ; 
+
+// syntax error also in the same case 
+// const z ; 
+// z=1 
+
