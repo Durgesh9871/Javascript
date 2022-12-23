add7f3c8800e68d4f15851d9399f40ccf12c9e21
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-23 16:57:51 +0530

    file

--- Interview_Closures/EventLoop.js
+++ Interview_Closures/EventLoop.js
@@ -20,14 +20,14 @@
 
 function x(){
    for(var i=0 ; i<5 ; i++){
-    function y(i){
+    // function y(i){
         setTimeout(()=>{
             console.log(i)
         },i * 1000)
      }
 
-     y(i)
-    }
+    //  y(i)
+    // }
 
 }
 x()
