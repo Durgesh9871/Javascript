44b08d44e34de021689889870d36e02739f5a465
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-04 12:05:21 +0530

    file

--- restOperator.js
+++ restOperator.js
@@ -1,8 +1,8 @@
 
 //  Its an example of rest operator 
 function rest(...other){
-    console.log(other)
-    return (a+b+c)
+    console.log(...other)
+    // return (a+b+c)
 }
 
 const ans = rest(1,2,3,4,5,6)
