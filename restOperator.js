5a5e97017382db8ebab36574aae4c4cec7d91f93
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-04 12:31:10 +0530

    file

--- restOperator.js
+++ restOperator.js
@@ -7,3 +7,18 @@ function rest(...other){
 
 const ans = rest(1,2,3,4,5,6)
 console.log(ans)
+
+
+
+
+//  EXAMPLE OF SPREAD OPERATOR -----------------------------------
+
+let name = [1,2,3,5,8,6]
+
+function spread(name){
+  
+    return name
+}
+
+const sum  = spread(...name)
+console.log("sum" , sum)
