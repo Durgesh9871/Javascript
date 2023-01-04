ae803a95e3fe223629dd2dc5adf7673e2128b296
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-04 12:50:43 +0530

    file

--- restOperator.js
+++ restOperator.js
@@ -1,17 +1,29 @@
 
 //  Its an example of rest operator 
-function rest(...other){
-    console.log(...other)
-    // return (a+b+c)
+//  array with 
+function rest(a,b,c,...other){
+    // console.log(...other)
+    return (a+b+c)
 }
 
 const ans = rest(1,2,3,4,5,6)
 console.log(ans)
 
+//  with objects 
+const students = {
+    names:"avni" , 
+    age:"12" ,
+    gender:"girl",
+}
+
+const {names , gender ,age} = students
+
+console.log('students' , names ,gender)
 
 
 
 //  EXAMPLE OF SPREAD OPERATOR -----------------------------------
+//  array with 
 
 let name = [1,2,3,5,8,6]
 
