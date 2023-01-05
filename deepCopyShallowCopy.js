e8e5a81fd07181cab60bb243073da4ccd2ab9de6
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-05 10:16:42 +0530

    file

--- deepCopyShallowCopy.js
+++ deepCopyShallowCopy.js
@@ -11,3 +11,14 @@ let copy = obj
 copy.name = "aman"
 
 console.log(obj)
+
+//  Shallow copy ------------------ 
+// In this its does not make the copy of memory allocation, Its makes the copy of value 
+let obj1 = {
+    name:"amisha"
+}
+let user = Object.assign({} , obj1) 
+user.name ="durgesh"  
+
+console.log(user)
+console.log('obj1' , obj1)
