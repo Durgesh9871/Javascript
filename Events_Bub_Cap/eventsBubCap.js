f47b5d89a0462a15a7649db0c87cd2b0db3086dd
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-05 12:59:05 +0530

    file

--- Events_Bub_Cap/eventsBubCap.js
+++ Events_Bub_Cap/eventsBubCap.js
@@ -1,5 +1,7 @@
 
-//  By default if we did not pass any value it act like 
+//  By default if we did not pass any value it act like event bubbling 
+// if we pass true then its act like event capturing 
+//  if we pass false then its act like event bubbling 
 document.querySelector("#child")
 .addEventListener('click' ,()=>{
     console.log('child')
