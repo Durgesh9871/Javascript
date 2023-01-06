11d0c75f378f728a1f75ece58abc25ee41dea318
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-06 19:43:26 +0530

    file

--- Interview_Closures/Promises.js
+++ Interview_Closures/Promises.js
@@ -14,47 +14,15 @@ let cart = ["shirt" , "pant" ,"shoes"]
 // const api = 'https://tan-real-buffalo.cyclic.app/Fish&SeaFood' 
 
 
-  async function foodData(){
-    try{
-        let res = await fetch('https://tan-real-buffalo.cyclic.app/Fish&SeaFood')
-        let data = await res.json()
-        console.log(data)
 
 
-        // try{
-        //     let res = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=ba12efd88a7cb987defdf207973de6df&language=en-US&page=1")
-        //     let data = await res.json()
-             
-        //     let real_data = data.results
-       
-        //      popular(real_data)
-        //     console.log(real_data)
-        //        }
-    }
-    catch(err){
-         console.log("err")
-    }
-   
 
-
-}
-
-const ans = foodData() 
-console.log(ans)
+ 
 
 
 
 
 
-//  function call(){
-//     console.log("world")
-//    payment(function (){
-//     console.log('hello')
-//   })
-   
-//  }
-//  const work = call()
-// //  work()
 
 
  
