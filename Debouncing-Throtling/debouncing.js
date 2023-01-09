2f269dec9aabbf31b05faa3713489c57ae73e2d6
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-10 08:54:36 +1400

    file

--- Debouncing-Throtling/debouncing.js
+++ Debouncing-Throtling/debouncing.js
@@ -4,3 +4,13 @@ let count = 0
 const getData = ()=>{
    console.log('data fetched', count++)
 }
+
+//  With debouncing ---------------------------- 
+
+const getDebouncing = ()=>{
+    return function(){
+        
+    }
+}
+
+const better = getDebouncing(getData , 300)
