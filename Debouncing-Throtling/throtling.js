0d783723de2a836c9eed617f199a97a249f884e3
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-10 09:26:57 +1400

    file

---
+++ Debouncing-Throtling/throtling.js
@@ -0,0 +1,5 @@
+
+//  In throtling we can ---limit the rate -- 
+
+// for example----
+//  there is a button and user is clicking on a button 10 times so we cant send api req times, So for this we use throtling like first api is called when the button is click on first time and the next req is called after certain time so we limit the  rate the of flow -----------
