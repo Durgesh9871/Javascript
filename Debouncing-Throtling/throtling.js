27e8326218ae1da894c50f5bc6f986788d9ae320
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-10 10:00:49 +1400

    file

--- Debouncing-Throtling/throtling.js
+++ Debouncing-Throtling/throtling.js
@@ -17,6 +17,10 @@ const throtling = (func , limit)=>{
        if(flag == true){
         func()
         flag = false 
+
+        setTimeout(()=>{
+         flag = true 
+        },limit)
        }
     }
 
