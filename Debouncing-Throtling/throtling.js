d4cb7305bd9c2a970ee16e82836779fbed024dee
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-10 09:51:41 +1400

    file

--- Debouncing-Throtling/throtling.js
+++ Debouncing-Throtling/throtling.js
@@ -3,3 +3,9 @@
 
 // for example----
 //  there is a button and user is clicking on a button 10 times so we cant send api req times, So for this we use throtling like first api is called when the button is click on first time and the next req is called after certain time so we limit the  rate the of flow -----------
+
+let count = 0 
+
+const IncreaseCount = ()=>{
+    
+}
