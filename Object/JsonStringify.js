b6bf162f0e3bb151dfb7beaf6786b19c0fcddd0a
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-11 16:59:18 +0530

    file

--- Object/JsonStringify.js
+++ Object/JsonStringify.js
@@ -15,7 +15,8 @@ console.log(obj)
 
 
 //  JSON PARSE IS HERE --------------------------------  
-  
+//   Defination :-   When receiving data from a web server, the data is always a string. Parse the data with JSON.parse() , and the data becomes a JavaScript object. 
+
 let parse = JSON.parse({
     "x":1 ,
     "y":2 , 
