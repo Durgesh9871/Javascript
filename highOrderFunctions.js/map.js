68ed8de712527b86a6823c3a1bf0534685a8bc10
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-11 17:32:45 +0530

    file

--- highOrderFunctions.js/map.js
+++ highOrderFunctions.js/map.js
@@ -2,7 +2,8 @@
 
 // Defination:- Map creates a new array by calling a function for each element once  in an array 
 // Map does not call the function when the array is empty ; 
-const myName = ["durgesh" , "aman" ,"radha" , "sima" , "amisha"]
+
+const myName = ["durgesh" , "aman" ,"radha" , "sima" , "amisha"] 
  
 
 let ans = myName.map((item)=>{
