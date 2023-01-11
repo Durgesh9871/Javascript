137a2d7e07fb13992cf20c98d4b7073eeb3d5c7f
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-11 16:36:22 +0530

    file

--- Object/JsonStringify.js
+++ Object/JsonStringify.js
@@ -1,13 +1,13 @@
 //  Json stringfify logic is here ---------------------------
 
 
-// let obj = 
-// JSON.stringify( {
-//     x:1 , 
-//     y:2 , 
-//     z:3 , 
-// })
-// console.log(JSON.stringify({ x: 5, y: 6 }));
+let obj = 
+JSON.stringify( {
+    x:1 , 
+    y:2 , 
+    z:3 , 
+})
+console.log(JSON.stringify({ x: 5, y: 6 }));
 
 
-// console.log(obj)
+console.log(obj)
