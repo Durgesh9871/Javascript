6b21cb989120c5bb90016132bf26fe92e849d9f3
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-11 17:40:22 +0530

    file

---
+++ highOrderFunctions.js/filter.js
@@ -0,0 +1,10 @@
+// ABOUT FILTER -------------------------------------------
+
+let number = [1,2,3,4,5,6,7,8,9] 
+
+
+let ans = number.filter((item)=>{
+    return item > 5 
+})
+
+console.log(ans)
