ce9deeed904d96a8604d45d09bdc3bb18191ce27
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-11 17:44:46 +0530

    file

--- highOrderFunctions.js/filter.js
+++ highOrderFunctions.js/filter.js
@@ -1,5 +1,7 @@
 // ABOUT FILTER -------------------------------------------
 
+// Filter is an higher order function which returns an array which is filled with those elements that passed the test 
+
 let number = [1,2,3,4,5,6,7,8,9] 
 
 
