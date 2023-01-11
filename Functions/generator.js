2a75a9bb140d3b1368d0feb86daa34ccf6423f56
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-11 20:09:50 +0530

    file

--- Functions/generator.js
+++ Functions/generator.js
@@ -5,6 +5,22 @@
 
 
 function* generate(){
+ let i = 0 
 
+ yield 1 
+ yield 2
+ 
+//   More optimised code can be --using a while loop -- 
+   
+// while(true){
+//     yield i++ 
+// }
 
 }
+
+const ans = generate()
+console.log(ans.next()) 
+//  We are getting a object by printing it ans.next()
+
+//  Defination :- In java Script a generator is a function  which returns an object on which we can call.next()
+
