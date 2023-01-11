e5ee0df65850d8796016e546abf6672fa5568a0b
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-11 12:53:58 +0530

    file

--- Object/objFreeze.js
+++ Object/objFreeze.js
@@ -1,8 +1,10 @@
-//  Object freeze code is here ---------------------
+//  Object freeze code is here --------------------- 
+//  with the help of obj.freeze() we can freezw the obj 
 let obj = {
     name:"durgesh" , 
     age:19, 
 }
+
 Object.freeze(obj) 
 
 obj.class = 7 ; 
