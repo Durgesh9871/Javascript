698082ce3bfc176017d5e4ced83fc2d3bedbea3b
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-11 16:52:48 +0530

    file

--- Object/JsonStringify.js
+++ Object/JsonStringify.js
@@ -7,9 +7,16 @@ JSON.stringify( {
     y:2 , 
     z:3 , 
 })
-console.log(JSON.stringify({ x: 5, y: 6 }));
+// console.log(JSON.stringify({ x: 5, y: 6 }));
 
 console.log(obj)
 
 
-//  JSON PARSE IS HERE --------------------------------
+//  JSON PARSE IS HERE --------------------------------  
+  
+
+let parse = JSON.parse({
+    "x":1 ,
+    "y":2 , 
+    "z":3 , 
+})
