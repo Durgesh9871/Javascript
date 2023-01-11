2630623df79725cee2cea901c5f5e32bc1a7dfe9
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-11 13:00:00 +0530

    file

--- Object/objFreeze.js
+++ Object/objFreeze.js
@@ -24,4 +24,6 @@ Object.seal(obj2)
 obj2.name = "raman"
 obj2.class = 7 ; 
 
-console.log(obj2)
+console.log(obj2)  
+
+//  So in obj.seal() new property cannot be added and old property cannot be removed 
