89a415209d9891c671ed6027202e38642c78c355
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-14 08:13:31 +0530

    file

--- Storage/Local.js
+++ Storage/Local.js
@@ -1,3 +1,10 @@
 // Both  Local storage and session storage are the part of web api's  ; 
 // 2. Web Store api are used by developers to store some data on the web browers ;  
-// 
+// 3. The data is nothing but a key value pairs ;  
+
+
+
+// SESSION STORAGE  ---------------------------
+
+// SESSION_STORAGE :- WHEN WE TALK ABOUT SESSION STORAGE THAT MEANS DATA IS IN THE IN STORAGE FOR THAT ONLY PARTICULAR SESSION ; 
+// for ex :- When the user visited the web page and we store some data in session storage, if the user closes the browser or the tab then we lost the data 
