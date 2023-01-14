60e6c969cd865253d98005c091913a87d3916fec
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-14 08:59:35 +0530

    file

--- Storage/Local.js
+++ Storage/Local.js
@@ -8,7 +8,7 @@
 
 // SESSION_STORAGE :- WHEN WE TALK ABOUT SESSION STORAGE THAT MEANS DATA IS IN THE IN STORAGE FOR THAT ONLY PARTICULAR SESSION ; 
 // 2. But it's very useful than cookies , Unlike cookies the data is not send to the server while makeing the network request ; 
-// 3. They can store a large amount of data compare than cookies; we can store something around 5mb of data in session storage , while coikies we can just store the 4000 bytes of data 
+// 3. They can store a large amount of data compare than cookies; we can store something around 5mb of data in session storage , while cookies we can just store the 4000 bytes of data 
 
 // for ex :- When the user visited the web page and we store some data in session storage, if the user closes the browser or that particular tab So we lost the data completely , 
 
@@ -20,3 +20,10 @@
 
 // LOCAL STORAGE IS SAME LIKE SESSION STORAGE BUT IT DOES NOT COME WITH EXPIERY, ITS DOES NOT CLEAR ITSELF ,EVEN WHEN THE TABS OR THE BROWSER CLOSE THE DATA IS STILL THERE 
 // 2. local storage has the higher memory capacity the session storage and the cookies ; It's varies on device to device but roufly it stores 5mb of data ; 
+
+
+
+
+
+// QUESTION :- WHY WE NEED TO STORE THE DATA IN LOCAL STORAGE OR SESSION STORAGE ?? 
+// ANS :-  We can also store the data in the database but getting the data from web Api storage is much more faster the getting the data by making a network request ; 
