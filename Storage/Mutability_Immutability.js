cfd9d917723c22866f83716c322e776f8d94ae07
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-14 09:45:14 +0530

    file

--- Storage/Mutability_Immutability.js
+++ Storage/Mutability_Immutability.js
@@ -1,1 +1,17 @@
-// Mutability :- 
+// Mutability :- Mutability is a type of variable that can changed in javascript only objects and array are mutable
+
+// Immutability  :- Immuatble means the state of the object can not be changed  
+
+                                    
+
+
+                                    // JAVASCRIPT DATA TYPES
+            // 1. PRIMITIVE DATA TYPE                       2.NON-PRIMITIVE DATA TYPE  
+                                      
+            // * NUMBER                                    * ARRAY 
+            // * BOOLEAN                                   * OBJECTS 
+            // * STRING                                    * FUNCTION , DATA , 
+            // * NULL 
+            // * UNDEFINED 
+            // * SYMBOL 
+                                            
