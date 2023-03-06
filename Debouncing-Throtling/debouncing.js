8b5bdd485678719ce687a8fbad59443e2d3bc254
Durgesh9871 <devisingh9871686098@gmail.com>
2023-03-06 18:10:34 +0530

    file

--- Debouncing-Throtling/debouncing.js
+++ Debouncing-Throtling/debouncing.js
@@ -6,34 +6,36 @@ const getData = ()=>{
 }
 
 //  With debouncing ---------------------------- 
-// const getDebouncing = (data , delay)=>{
-//     let timer ; 
-//     return function(){
-//         clearTimeout(timer) ; 
-//       timer =  setTimeout(()=>{
-//            data()
-//         },delay)
-//     }
-// }
+const getDebouncing = (data , delay)=>{
+    let timer ; 
+    return function(){
+        clearTimeout(timer) ; 
+      timer =  setTimeout(()=>{
+           data()
+        
+        },delay)
+    }
+}
 
 
 //  we can implement debouncing like this also but the chance is the environment and lexical scope is not 
 // correct and might have chance same function is not called with same arguments------------- 
 
 
-const getDebouncing = (fn , delay)=>{
-    let timer ; 
-    return function(){
-        clearTimeout(timer) ; 
-      timer =  setTimeout(()=>{
-        let context = this ,
-         args = arguments 
-           getData.apply(context , arguments)
-        },delay)
-    }
-}
+// const getDebouncing = (fn , delay)=>{
+//     let timer ; 
+//     return function(){
+//         clearTimeout(timer) ; 
+//       timer =  setTimeout(()=>{
+//         let context = this ,
+//          args = arguments 
+//            getData.apply(context , arguments)
+//         },delay)
+//     }
+// }
 //  So here we use context and args so the lexical scope and environment are correct and same function is called .
 
 //  Use upper way for demonstration ------------------------------------------------> 
 
 const better = getDebouncing(getData , 1000)
+better()
